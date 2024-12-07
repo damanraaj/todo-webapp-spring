bafaf6a756ff129fdfde18fceb4a5f3f27e7fe1d
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-12-07 23:29:13 +0530

    create login endpoint

--- src/main/java/com/daman/myfirstwebapp/hello/SayHelloController.java
+++ src/main/java/com/daman/myfirstwebapp/hello/SayHelloController.java
@@ -1,4 +1,4 @@
-package com.daman.myfirstwebapp;
+package com.daman.myfirstwebapp.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
