12732ea2bf839dcddf1ac59da9af12831eb23c3a
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-12-09 17:03:53 +0530

    added logger to login controller

--- src/main/java/com/daman/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/daman/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.daman.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping("login")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
+        logger.debug("Login request with param - {}", name);
         return "login";
     }
 }
